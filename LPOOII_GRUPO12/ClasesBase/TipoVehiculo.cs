9241da610c54a77d3a14b7cc8376e49e8174dd58
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-09-07 11:32:27 -0300

    Atributos y propiedades de las clases terminado - falta la cardinalidad - Archivo Vistas Creado

--- LPOOII_GRUPO12/ClasesBase/TipoVehiculo.cs
+++ LPOOII_GRUPO12/ClasesBase/TipoVehiculo.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClasesBase
+{
+    public class TipoVehiculo
+    {
+        private int tv_TVCodigo;
+        private string tv_Descripcion;
+        private decimal tv_Tarifa;
+
+        public decimal Tv_Tarifa
+        {
+            get { return tv_Tarifa; }
+            set { tv_Tarifa = value; }
+        }
+
+        public string Tv_Descripcion
+        {
+            get { return tv_Descripcion; }
+            set { tv_Descripcion = value; }
+        }
+
+        public int Tv_TVCodigo
+        {
+            get { return tv_TVCodigo; }
+            set { tv_TVCodigo = value; }
+        }
+    }
+}
--- LPOOII_GRUPO12/ClasesBase/Usuario.cs
+++ LPOOII_GRUPO12/ClasesBase/Usuario.cs
@@ -7,5 +7,47 @@ namespace ClasesBase
 {
     public class Usuario
     {
+        private string usr_UserName;
+        private string usr_Password;
+        private string usr_Apellido;
+        private string usr_Nombre;
+        private int rol_id;
+        private int usr_Id;
+
+        public int Usr_Id
+        {
+            get { return usr_Id; }
+            set { usr_Id = value; }
+        }
+
+        public int Rol_id
+        {
+            get { return rol_id; }
+            set { rol_id = value; }
+        }
+
+        public string Usr_Nombre
+        {
+            get { return usr_Nombre; }
+            set { usr_Nombre = value; }
+        }
+
+        public string Usr_Apellido
+        {
+            get { return usr_Apellido; }
+            set { usr_Apellido = value; }
+        }
+
+        public string Usr_Password
+        {
+            get { return usr_Password; }
+            set { usr_Password = value; }
+        }
+
+        public string Usr_UserName
+        {
+            get { return usr_UserName; }
+            set { usr_UserName = value; }
+        }
     }
 }
