e7cb608e54d1cac4877a09b4de0bd2b35dedbf03
Nicolas Santiago Velasquez <nicolasyago9@gmail.com>
2023-10-30 12:57:55 -0300

    TP3 - Punto 2 a y b

---
+++ LPOOII_GRUPO12/ClasesBase/TrabajarTiposVehiculo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    public class TrabajarTiposVehiculo
+    {
+
+        public static DataTable TraerTiposVehiculo() 
+        {
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.playaConnection);
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "SELECT *";
+
+            cmd.CommandText += " FROM TipoVehiculo";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            //Ejecuta la consulta
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+            //Llena los datos de la consulta en el DataTable
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
+
+    }
+}
