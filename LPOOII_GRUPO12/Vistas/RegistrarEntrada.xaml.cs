c1297734ce997aaef423b3e58e6d8707ecd9d195
Martinchx <martincruz1426@gmail.com>
2023-11-20 02:24:01 -0300

    TP5 - Punto 2a

---
+++ LPOOII_GRUPO12/Vistas/RegistrarEntrada.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using ClasesBase;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for RegistrarEntrada.xaml
+    /// </summary>
+    public partial class RegistrarEntrada : Window
+    {
+        public RegistrarEntrada()
+        {
+            InitializeComponent();
+
+            // Cargar tipos de vehículo en el ComboBox
+            CargarTiposVehiculo();
+        }
+
+        private void CargarTiposVehiculo()
+        {
+            try
+            {
+                DataTable dtTiposVehiculo = TrabajarTiposVehiculo.TraerTiposVehiculo();
+
+                comboTiposVehiculo.ItemsSource = dtTiposVehiculo.DefaultView;
+                comboTiposVehiculo.DisplayMemberPath = "tv_Descripcion";
+                comboTiposVehiculo.SelectedValuePath = "tv_TVCodigo";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar tipos de vehículo: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void btnRegistrarEntrada_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Entrada registrada con éxito", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
